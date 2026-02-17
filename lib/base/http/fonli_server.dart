90bfbef6da42585fe3edd9460dcc9cab92c5125c
Victor Gaudiot <victor.gaudiot@uber.com>
2026-02-17 09:53:27 -0300

    foreign to native exercise http request

--- lib/base/http/fonli_server.dart
+++ lib/base/http/fonli_server.dart
@@ -5,12 +5,25 @@ import 'package:fonli_app/core/types/response.type.dart';
 class FonliServer {
   static const String baseUrl = "http://localhost:8000";
 
-  static Future<Result<WordTranslationExercise, Exception>> getWordTranslationExercise() async {
+  static Future<Result<WordTranslationExercise, Exception>> getWordTranslationNativeToForeignExercise() async {
     Dio dio = Dio();
     final response = await dio.get('$baseUrl/word-translation/native-to-foreign');
 
     if (response.statusCode != 200) {
       return Result.error(Exception("Failed to get word translation exercise"));
+    }
+
+    final WordTranslationExercise exercise = WordTranslationExercise.fromJson(response.data);
+
+    return Result.ok(exercise);
+  }
+
+  static Future<Result<WordTranslationExercise, Exception>> getWordTranslationForeignToNativeExercise() async {
+    Dio dio = Dio();
+    final response = await dio.get('$baseUrl/word-translation/foreign-to-native');
+
+    if (response.statusCode != 200) {
+      return Result.error(Exception("Failed to submit word translation exercise"));
     }
 
     final WordTranslationExercise exercise = WordTranslationExercise.fromJson(response.data);
